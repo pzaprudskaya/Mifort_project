e947c02c928231af76e0488ef3f4ebf24fc53305
Bragodin <drozdegor.sr@gmail.com>
2019-08-02 02:49:33 +0300

    menu select

--- src/app/core/logo-user-company/logo-user-company.component.ts
+++ src/app/core/logo-user-company/logo-user-company.component.ts
@@ -1,7 +1,7 @@
+import { UserService } from './../../user.service';
 import {Component, Input, OnInit} from '@angular/core';
 import {EmployeesProfileService} from '../../profile-page/profile/employees-profile.service';
 import {TableService} from '../../components/table/table.service';
-
 @Component({
   selector: 'app-logo-user-company',
   templateUrl: './logo-user-company.component.html',
@@ -11,11 +11,33 @@ export class LogoUserCompanyComponent implements OnInit {
   @Input() name: string;
   @Input() value: string;
   photo: string;
-  constructor(private employeesProfileService: EmployeesProfileService, private tableService: TableService) { }
+  users = ['Polina Zaprudskaya', 'Egor Drozd', 'Vasiliy Pupanov'];
+  companys: string[] = ['2', '232'];
+  option: string = this.users[0];
+  optionCompany: string = this.companys[0];
 
+  user;
+
+
+  constructor(private employeesProfileService: EmployeesProfileService, 
+    private tableService: TableService,
+    private userService: UserService) { }
+
+    changeName(){   
+      this.userService.getUser(this.option).subscribe(
+        user => {
+          this.user = user[0];
+          this.companys = this.user.company;
+        });
+      
+      console.log(this.user);
+    }
+    changeProject(){
+      console.log(this.optionCompany)
+    }
+  
   ngOnInit() {
     this.photo = '';
-
     this.name = 'Polina Zaprudskaya';
     this.employeesProfileService.getName(this.name);
     this.value = 'Mifort';

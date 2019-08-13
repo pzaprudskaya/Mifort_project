7df1301d672a90db446b31a8dbe4972dbbfd60fc
Bragodin <drozdegor.sr@gmail.com>
2019-08-14 00:14:38 +0300

    signIn and signUp

--- src/app/authorization/sign-up/sign-up.component.ts
+++ src/app/authorization/sign-up/sign-up.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService, GoogleLoginProvider } from 'angular5-social-login';
+import { User } from '../authorization.model';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -8,8 +11,10 @@ import { AuthService, GoogleLoginProvider } from 'angular5-social-login';
 })
 export class SignUpComponent implements OnInit {
   signForm: FormGroup;
-  type: string = "password"
-  flag: boolean = false
+  type: string = "password";
+  flag: boolean = false;
+  user: User;
+  authPrompt: boolean;
   ngOnInit() {
     this.signForm = new FormGroup({
       "fullNameControl": new FormControl('', [Validators.required, Validators.minLength(2)]),
@@ -19,16 +24,29 @@ export class SignUpComponent implements OnInit {
       "passwordControl": new FormControl('', [Validators.required, Validators.minLength(6)
       ]),
     });
+    this.user = {
+      id: 'frfre',
+      email: this.signForm.value.emailControl,
+      name: this.signForm.value.fullNameControl,
+      image: 'https://www.deadline.com.ua/design/img/default-avatar.png',
+      password: this.signForm.value.passwordControl
+    }
   }
-  constructor( private socialAuthService: AuthService ) {}
+  constructor( private socialAuthService: AuthService,  private router: Router ) {}
 
-  public socialSignIn(socialPlatform : string) {
+  public socialSignUn(socialPlatform : string) {
     let socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
     this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-        console.log(socialPlatform + " sign in data : " , userData);
+      userData => {
+        // console.log(socialPlatform + " sign in data : " , userData);
+        this.user.id = userData.id;
+        this.user.email = userData.email;
+        this.user.name = userData.name;
+        this.user.image = userData.image;
+        this.user.password = userData.email;
       }
     );
+    
   }
   showPassword(){
     if(this.type == "password") {
@@ -38,6 +56,20 @@ export class SignUpComponent implements OnInit {
       this.type = "password"
       this.flag = false
     }
-    
+  }
+  checkRegistration(){
+    this.user = {
+      id: 'frfre',
+      email: this.signForm.value.emailControl,
+      name: this.signForm.value.fullNameControl,
+      image: 'https://www.deadline.com.ua/design/img/default-avatar.png',
+      password: this.signForm.value.passwordControl
+    }
+    if(this.signForm.status === "VALID"){
+      this.router.navigate(['/profile']);
+    }
+    else {
+      this.authPrompt = true;
+    }
   }
 }
